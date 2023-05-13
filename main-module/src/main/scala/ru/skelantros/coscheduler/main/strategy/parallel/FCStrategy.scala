7eaef8f505a4104b2176ac34d320c3dd52e115db
skelantros <ad3fful@yandex.ru>
2023-05-13 18:24:11 +0300

    Hybrid FCS strategy first version

--- main-module/src/main/scala/ru/skelantros/coscheduler/main/strategy/parallel/FCStrategy.scala
+++ main-module/src/main/scala/ru/skelantros/coscheduler/main/strategy/parallel/FCStrategy.scala
@@ -5,16 +5,15 @@ import cats.implicits._
 import ru.skelantros.coscheduler.logging.DefaultLogger
 import ru.skelantros.coscheduler.main.Configuration
 import ru.skelantros.coscheduler.main.implicits._
+import ru.skelantros.coscheduler.main.strategy.combination._
 import ru.skelantros.coscheduler.main.system.{SchedulingSystem, WithTaskSpeedEstimate}
 import ru.skelantros.coscheduler.model.{Node, Task}
 
-import scala.collection.immutable.{BitSet, TreeSet}
+import scala.collection.immutable.TreeSet
 import scala.concurrent.duration.DurationInt
 
 class FCStrategy(val schedulingSystem: SchedulingSystem with WithTaskSpeedEstimate,
                  val config: Configuration) extends ParallelStrategy with DefaultLogger {
-
-    import FCStrategy._
 
     private val measurementTime = config.taskSpeed.flatMap(_.measurement).getOrElse(250.millis)
     private val waitBeforeMeasurementTime = config.taskSpeed.flatMap(_.waitBeforeMeasurement).getOrElse(100.millis)
@@ -24,17 +23,7 @@ class FCStrategy(val schedulingSystem: SchedulingSystem with WithTaskSpeedEstima
         NodeWorker(node).execute(tasks)
 
     private case class NodeWorker(node: Node) {
-        private def measureCombinationSpeed(combination: Combination): IO[CombinationWithSpeed] = for {
-            _ <- log.debug(node.id)(s"started measuring combination $combination")
-            runTasks <- combination.genParMap(_.toList)(schedulingSystem.saveResumeTask)
-            _ <- IO.unit.delayBy(waitBeforeMeasurementTime) // возможно не нужно
-            taskSpeeds <- runTasks.parMap(schedulingSystem.speedOf(measurementTime, measurementAttempts))
-            _ <- log.debug(node.id)(s"taskSpeeds = $taskSpeeds")
-            _ <- runTasks.parMap(schedulingSystem.savePauseTask)
-        } yield CombinationWithSpeed(combination, taskSpeeds.sum)
-
-        private def measureCombinationSpeeds(tasks: Vector[Task.Created]): IO[TreeSet[CombinationWithSpeed]] =
-            makeCombinations(tasks).map(measureCombinationSpeed).toList.sequence.map(TreeSet.from(_))
+        private val measurer = new SpeedMeasurer(log.debug(node.id)(_), schedulingSystem, measurementTime, measurementAttempts, waitBeforeMeasurementTime)
 
         private def raceCallbacks(callbacks: Seq[IO[Unit]]): IO[Unit] = callbacks.foldLeft(IO.never[Unit]) {
             case (io, curCallback) => io.race(curCallback) >> IO.unit
@@ -73,7 +62,7 @@ class FCStrategy(val schedulingSystem: SchedulingSystem with WithTaskSpeedEstima
                     pause <- schedulingSystem.savePauseTask(start)
                 } yield pause
             }
-            combinations <- measureCombinationSpeeds(tasks)
+            combinations <- measurer.measureCombinationSpeeds(tasks)
             action <- go(tasks.toSet, combinations)
         } yield action
     }
@@ -82,15 +71,4 @@ class FCStrategy(val schedulingSystem: SchedulingSystem with WithTaskSpeedEstima
 object FCStrategy {
     def apply(schedulingSystem: SchedulingSystem with WithTaskSpeedEstimate, config: Configuration): FCStrategy =
         new FCStrategy(schedulingSystem, config)
-
-    private def makeCombination[A](coll: IndexedSeq[A])(idxs: Set[Int]): Set[A] = idxs map coll
-
-    private def makeCombinations[A](coll: IndexedSeq[A]): IndexedSeq[Set[A]] = for {
-        x <- 1 until 1 << coll.size
-        bitSet = BitSet.fromBitMask(Array(x.toLong))
-    } yield makeCombination(coll)(bitSet)
-
-    private type Combination = Set[Task.Created]
-    private case class CombinationWithSpeed(combination: Combination, speed: Double)
-    private implicit val ordering: Ordering[CombinationWithSpeed] = Ordering.by(_.speed)
 }
